2d50131bbcdaee5357119917fa6c381f54c42fe4
Lovász Ákos <akos4x44@gmail.com>
2021-12-12 17:02:27 +0100

    did 2nd assignment

---
+++ 2. Feladat/forraskod.py
@@ -0,0 +1,29 @@
+from os import read
+
+docTitle = "GotoMoCap QNSZT Modellkollekció tartalma\n\n"
+
+with open("objExport.obj",'r') as fi:
+    id = []
+    for ln in fi:
+        if ln.startswith("#") and (ln.__contains__("object") or ln.__contains__("polygons")):
+            id.append(ln[2:])
+
+for ln in id:
+    finalArray = []
+    for ln in id:
+        ln = ln.replace("object","Név:")
+        ln = ln.replace("polygons","polygon")
+        ln = ln.replace("triangles","háromszög")
+        ln = ln.replace('\n','')
+        finalArray.append(ln)
+
+
+with open('docExport.txt', 'w', encoding='utf8') as f:
+    f.write(docTitle)
+    for item in range(len(finalArray)):
+        if item % 2 == 0:
+            f.write("%s: " % finalArray[item])
+        else:
+            f.write("%s\n" % finalArray[item])
+
+print('done :)')
